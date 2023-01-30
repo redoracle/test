7b8876783fe7ae6001ab3863c89f3767306bada8
Michael <michael.dogali@gmail.com>
2023-01-30 17:12:04 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+trap 'killall -s SIGTERM cardano-node' SIGINT SIGTERM
+# "docker run --init" to enable the docker init proxy
+# To manually test: docker kill -s SIGTERM container
+
+head -n 8 ~/.scripts/banner.txt
+
+. ~/.bashrc > /dev/null 2>&1
+
+echo "NETWORK: $NETWORK $POOL_NAME $TOPOLOGY";
+
+[[ -z "${CNODE_HOME}" ]] && export CNODE_HOME=/opt/cardano/cnode 
+[[ -z "${CNODE_PORT}" ]] && export CNODE_PORT=6000
+
+echo "NODE: $HOSTNAME - Port:$CNODE_PORT - $POOL_NAME";
+cardano-node --version;
+
+# Customisation 
+customise () {
+find /opt/cardano/cnode/files -name "*config*.json" -print0 | xargs -0 sed -i 's/127.0.0.1/0.0.0.0/g' > /dev/null 2>&1 
+grep -i ENABLE_CHATTR /opt/cardano/cnode/scripts/cntools.sh >/dev/null && sed -i 's/ENABLE_CHATTR=true/ENABLE_CHATTR=false/g' /opt/cardano/cnode/scripts/cntools.sh > /dev/null 2>&1
+grep -i ENABLE_DIALOG /opt/cardano/cnode/scripts/cntools.sh >/dev/null && sed -i 's/ENABLE_DIALOG=true/ENABLE_DIALOG=false/' /opt/cardano/cnode/scripts/cntools.sh >> /opt/cardano/cnode/scripts/cntools.sh
+find /opt/cardano/cnode/files -name "*config*.json" -print0 | xargs -0 sed -i 's/\"hasEKG\": 12788,/\"hasEKG\": [\n    \"0.0.0.0\",\n    12788\n],/g' > /dev/null 2>&1
+return 0
+}
+
+export UPDATE_CHECK='N'
+
+if [[ "$NETWORK" == "mainnet" ]]; then
+  $CNODE_HOME/scripts/guild-deploy.sh -n mainnet -u -s f \
+  && customise \
+  && exec $CNODE_HOME/scripts/cnode.sh
+elif [[ "$NETWORK" == "preprod" ]]; then
+  $CNODE_HOME/scripts/guild-deploy.sh -n preprod -u -s f > /dev/null 2>&1 \
+  && customise \
+  && exec $CNODE_HOME/scripts/cnode.sh
+elif [[ "$NETWORK" == "preview" ]]; then
+  $CNODE_HOME/scripts/guild-deploy.sh -n preview -u -s f > /dev/null 2>&1 \
+  && customise \
+  && exec $CNODE_HOME/scripts/cnode.sh
+elif [[ "$NETWORK" == "guild-mainnet" ]]; then
+  $CNODE_HOME/scripts/guild-deploy.sh -n mainnet -u -s f > /dev/null 2>&1 \
+  && bash /home/guild/.scripts/guild-topology.sh > /dev/null 2>&1 \
+  && export TOPOLOGY="${CNODE_HOME}/files/guildnet-topology.json" \
+  && customise \
+  && exec $CNODE_HOME/scripts/cnode.sh
+elif [[ "$NETWORK" == "guild" ]]; then
+  $CNODE_HOME/scripts/guild-deploy.sh -n guild -u -s f > /dev/null 2>&1 \
+  && customise \
+  && exec $CNODE_HOME/scripts/cnode.sh
+else
+  echo "Please set a NETWORK environment variable to one of: mainnet / preview / preprod / guild-mainnet / guild"
+  echo "mount a '$CNODE_HOME/priv/files' volume containing: mainnet-config.json, mainnet-shelley-genesis.json, mainnet-byron-genesis.json, and mainnet-topology.json "
+  echo "for active nodes set POOL_DIR environment variable where op.cert, hot.skey and vrf.skey files reside. (usually under '${CNODE_HOME}/priv/pool/$POOL_NAME' ) "
+  echo "or just set POOL_NAME environment variable (for default path). "
+fi
